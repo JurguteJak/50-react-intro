09f600459d999c1b23390c9be3067c102b8ed13c
JurguteJak <jurgutejak@gmail.com>
2024-08-06 11:09:35 +0300

    components

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+export function Header() {
+    return (
+        <header>
+            <img src="" alt="Logo" />
+            <nav>
+                <a href="#">Guide</a>
+                <a href="#">Config</a>
+                <a href="#">Plugins</a>
+                <a href="#">Resources</a>
+                <a href="#">Version</a>
+            </nav>
+        </header>
+    );
+}
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,17 @@
+export function Hero() {
+    return (
+        <section>
+          <div>
+            <h1>Vite next generation frontend tooling</h1>
+            <p>Get ready for a development environment that can finally catch up with you.</p>
+            <div>
+              <a href="#">Get started</a>
+              <a href="#">Why Vite?</a>
+              <a href="#">View on Github</a>
+              <a href="#">⚡ViteConf 24!</a>
+            </div>
+          </div>
+          <img src="" alt="Vite image" />
+        </section>
+    );
+}
